6a55286d5d6e37b1c638d019839031f08e035fc2
Shawn dratvin <sdratvin@gmail.com>
2023-01-26 09:07:33 +0200

    “MongoDB”

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const app=express();
+const moragan = require('morgan');
+const cors = require('cors');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(moragan('dev'));
+app.use(cors());
+const Productrouter = require("./api/v1/routes/product");
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const Conn_str = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.lssdz12.mongodb.net/ecomdb`;
+mongoose.connect(Conn_str);
+app.use("/product", Productrouter);
+module.exports=app;
